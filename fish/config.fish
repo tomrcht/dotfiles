51b69c7b1a41d8e2d6cbaca552ec6269142edad6
Tom Rochat <tom.roch4t@gmail.com>
2019-06-03 11:27:24 +0200

    :)

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,13 @@
-set -g -x EDITOR "nvim"
-set -g -x USER_NICKNAME "tom"
-set -g -x PAGER "most"
+set -g -x EDITOR 'nvim'
+set -g -x USER_NICKNAME 'tom'
+set -g -x PAGER 'most'
 
 set -g -x GOPATH $HOME/development/go
-set -g -x FLUTTERPATH $HOME/development/fluttersdk
 set -g -x EPITECH_PATH $HOME/development/epitech
-set -g -x PATH /bin /sbin /usr/bin /usr/sbin $GOPATH/bin $FLUTTERPATH/bin $PATH
+set -g -x PATH /bin /sbin /usr/local/sbin /usr/bin /usr/sbin $GOPATH/bin $PATH
 
 source /Users/tom/.config/fish/aliases
-set fish_greeting ""
+set fish_greeting ''
 
 thefuck --alias | source
 
